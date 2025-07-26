648206747d0e66e21be71c0de2a73a6f342ba63b
NourSobhy1 <noursobhy222@gmail.com>
2025-07-26 20:39:02 +0300

    Entities

---
+++ OrderManagementSystem.Domain/Entities/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Domain.Entities
+{
+   public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
---
+++ OrderManagementSystem.Domain/Entities/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagementSystem.Domain.Entities
+{
+   public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string PaymentMethod { get; set; }
+        public string Status { get; set; } // Pending, Shipped, Delivered
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+        public Invoice Invoice { get; set; }
+    }
+}
